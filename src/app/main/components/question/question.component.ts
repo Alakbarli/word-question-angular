53db040101455f8663e413ae808ce4ef4414f6a5
Ulvi Alakbarli <ulvialakbarli@gmail.com>
2023-07-19 01:10:38 +0400

    completed question page

--- src/app/main/components/question/question.component.ts
+++ src/app/main/components/question/question.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { LanguageService } from '../../services/language.service';
 import { Word } from 'src/app/models/word';
 import { Unit } from 'src/app/models/unit';
+import { Language } from 'src/app/models/language';
+import { Languages } from '../../const/languages';
 
 @Component({
   selector: 'app-question',
@@ -10,14 +12,24 @@ import { Unit } from 'src/app/models/unit';
 })
 export class QuestionComponent implements OnInit {
   currentWordNumber:number=0;
-  selectedUnit:number=0;
-  slecetedLang:string="";
-  known:string="";
-  unknown:string="";
-  answer:string="";
+  selectedUnit:Array<number>=[];
+  slecetedLang:number=0;
+
+  askedWord:string="";
+  answeredWord:string="";
+  correctAnswer:string="";
+
   showAnswer:boolean=false;
+
   words:Array<Word>;
   units:Array<Unit>;
+  langList:Array<Language>=[
+    new Language(Languages.Azərbaycan,Languages[Languages.Azərbaycan]),
+    new Language(Languages.İngilis,Languages[Languages.İngilis])
+  ];
+
+  submit:boolean=false;
+  isCorrect:boolean=true;
 
   constructor(private langService:LanguageService) { }
 
@@ -26,51 +38,62 @@ export class QuestionComponent implements OnInit {
     this.words=this.langService.db.Words;
   }
 
+  checkAnswer() {
+    if(this.answeredWord&&this.answeredWord.trim()){
+        this.isCorrect= this.correctAnswer.split(",")
+        .some(x=>this.answeredWord.split(",")
+        .some(y=>y.trim().toLowerAz()==x.trim().toLowerAz()));
+        this.submit=true;
+    }
+  }
+  changeAnswer(){
+    this.submit=false;
+  }
+  showAnswerText(){
+    this.showAnswer=!this.showAnswer;
+  }
+
   GenerateRandomNumber (min:number, max:number){
     return Math.floor(Math.random() * (max - min+1) + min);
   };
-
   GenerateWordForQustion(){
-    let words=((this.filterUnitId>0)?this.langService.db.findWordByUnitId(this.filterUnitId):this.langService.db.Words);
-    if(words.length>0){
+    this.answeredWord="";
+    this.correctAnswer="";
+    this.showAnswer=false;
+    this.askedWord="";
+    this.submit=false;
+    let _words=this.words.filter(
+      w=>((this.selectedUnit&&this.selectedUnit.length>0)?this.selectedUnit?.includes(+w.unitId):true)
+      );
+    if(_words.length>0){
         let rnm=0;
-        if(words.length>1){
-            rnm=this.GenerateRandomNumber(0,words.length);
+        if(_words.length>1){
+            rnm=this.GenerateRandomNumber(0,_words.length-1);
             while(rnm==this.currentWordNumber){
-                rnm=this.GenerateRandomNumber(0,words.length);
+                rnm=this.GenerateRandomNumber(0,_words.length-1);
             }
         }
-        let word=words[rnm];
+        let word=_words[rnm];
         this.currentWordNumber=rnm;
-        let first;
-        let last;
         if(word){
-          this.answer="";
-          this.showAnswer=false;
-              // $("#word-question .main-word .last").addClass("d-none");
-              // $("#word-question .main-word svg").removeClass("d-none");
-              // $("#word-question .input .check-answer").removeClass("d-none");
-              // $("#word-question .input").removeClass("d-none");
-              // if(lang==0){
-              //    let rndmN=generateRandomNumber(0,2);
-              //    if(rndmN==1){
-              //        first=  word.nameAz;
-              //        last=  word.nameEn;
-              //     }
-              //     else{
-              //         last=  word.nameAz;
-              //          first=  word.nameEn;}
-              //   }
-              //   else if(lang=="az"){
-              //        first=  word.nameAz;
-              //        last=  word.nameEn;
-              //   }
-              //   else{
-              //        last=  word.nameAz;
-              //        first=  word.nameEn;
-              //   }  
-              // $("#word-question .main-word .first").text(first);
-              // $("#word-question .main-word .last").text(last);
+              if(this.slecetedLang==0){
+                 let rndmN=this.GenerateRandomNumber(0,2);
+                 if(rndmN==1){
+                     this.askedWord=  word.nameAz;
+                     this.correctAnswer=  word.nameEn;
+                  }
+                  else{
+                    this.correctAnswer=  word.nameAz;
+                      this.askedWord=  word.nameEn;}
+                }
+                else if(this.slecetedLang==Languages.Azərbaycan){
+                  this.askedWord=  word.nameAz;
+                  this.correctAnswer=  word.nameEn;
+                }
+                else{
+                  this.correctAnswer=  word.nameAz;
+                  this.askedWord=  word.nameEn;
+                }
         }
     }
     
---
+++ src/app/models/language.ts
@@ -0,0 +1,8 @@
+export class Language {
+    constructor(_id:number,_name:string) {
+        this.id=_id;
+        this.name=_name;
+    }
+    id:number;
+    name:string;
+}
